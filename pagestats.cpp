30e67d76882ba450ddc38cf1e6e33371af7d5edb
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-04-15 21:55:33 +0100

    first code commit

---
+++ pagestats.cpp
@@ -0,0 +1,56 @@
+
+
+#define BUFFSZ 512
+
+static char outputfile[BUFFSZ];
+
+
+void usage()
+{
+	cout << "USAGE:\n";
+	cout << "pagestats [control file] [output file]\n";
+}
+
+int main(int argc, char* argv[])
+{
+	FILE* inXML;
+	char data[BUFFSZ]; 
+	size_t len = 0;
+	int done;	
+
+	if (argc < 3) {
+		usage();
+		exit(-1);
+	}
+
+	strcpy(outputfile, argv[2]);
+
+	XML_Parser p_ctrl = XML_ParserCreate("UTF-8");
+	if (!p_ctrl) {
+		fprintf(stderr, "Could not create parser\n");
+		exit(-1);
+	}
+
+	XML_SetStartElementHandler(p_ctrl, starthandler);
+	inXML = fopen(argv[1], "r");
+	if (inXML == NULL) {
+		fprintf(stderr, "Could not open %s\n", argv[1]);
+		XML_ParserFree(p_ctrl);
+		exit(-1);
+	}
+
+	do {
+		len = fread(data, 1, sizeof(data), inXML);
+		done = len < sizeof(data);
+
+		if (XML_Parse(p_ctrl, data, len, 0) == 0) {
+			enum XML_Error errcde = XML_GetErrorCode(p_ctrl);
+			printf("ERROR: %s\n", XML_ErrorString(errcde));
+			printf("Error at column number %lu\n",
+				XML_GetCurrentColumnNumber(p_ctrl));
+			printf("Error at line number %lu\n",
+				XML_GetCurrentLineNumber(p_ctrl));
+			exit(-1);
+		}
+	} while(!done);
+
