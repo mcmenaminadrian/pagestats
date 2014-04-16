21cbb78e8866379d6ff64b91eaee82f91fcfd3af
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-04-16 08:27:10 +0100

    I'm on the train

--- pagestats.cpp
+++ pagestats.cpp
@@ -1,14 +1,67 @@
-
+#include <iostream>
+#include <set>
+#include <cstdlib>
+#include <cstdio>
+#include <expat.h>
 
 #define BUFFSZ 512
 
 static char outputfile[BUFFSZ];
 
+class PageSegment
+{
+	public:
+	PageSegment(int seg) {segment = seg;};
+	const int getSegment() const;
+	void incrementCount();
+	const int getCount() const;
+
+	private:
+	int segmentNumber;
+	int count;
+}
 
 void usage()
 {
 	cout << "USAGE:\n";
 	cout << "pagestats [control file] [output file]\n";
+}
+
+static void 
+countThread(int threadID, char* threadPath,
+	set<PageSegment>& overallCount, set<PageSegment>& memoryCount,
+	set<PageSegment>& codeCount>)
+{
+}
+
+static void XMLCALL
+	fileHandler(void *data, const XML_Char *name, const XML_Char **attr)
+{
+
+	//lots of different sets
+	set<PageSegment> overallCount;
+	set<PageSegment> memoryCount;
+	set<PageSegment> codeCount;
+	
+	int i;
+	int threadID = 0;
+	char threadPath[BUFFSZ]; 
+	if (strcmp(name, "file") == 0) {
+		for (i = 0; attr[i]; i += 2) {
+			if (strcmp(attr[i], "thread") == 0) {
+				threadID = atoi(attr[i + 1]);
+				break;
+			}
+		}
+		for (i = 0; attr[i]; i += 2) {
+			if (strcmp(attr[i], "path") == 0) {
+				strcpy(threadPath, attr[i + 1]);
+				break;
+			}
+		}
+		countThread(threadID, threadPath, overallCount, memoryCount,
+			codeCount);
+	} 
 }
 
 int main(int argc, char* argv[])
@@ -31,7 +84,7 @@ int main(int argc, char* argv[])
 		exit(-1);
 	}
 
-	XML_SetStartElementHandler(p_ctrl, starthandler);
+	XML_SetStartElementHandler(p_ctrl, fileHandler);
 	inXML = fopen(argv[1], "r");
 	if (inXML == NULL) {
 		fprintf(stderr, "Could not open %s\n", argv[1]);
